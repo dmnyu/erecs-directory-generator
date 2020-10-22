43cd688b5e7145ab127b41782e5a676df393ee69
Donald Mennerich <don.mennerich@nyu.edu>
2020-10-22 13:33:32 -0400

    formatting

--- main.go
+++ main.go
@@ -8,25 +8,25 @@ import (
 )
 
 type WorkOrderEntry struct {
-	ResourceID 				string
-	RefId					string
-	URI 					string
-	ContainerIndicator1		string
-	ContainerIndicator2		string
-	ContainerIndicator3		string
-	Title 					string
-	ComponentID				string
+	ResourceID          string
+	RefId               string
+	URI                 string
+	ContainerIndicator1 string
+	ContainerIndicator2 string
+	ContainerIndicator3 string
+	Title               string
+	ComponentID         string
 }
 
 func main() {
-   workOrder, err := openWorkOrder("digitization_work_order_report.tsv")
-   if err != nil {
-   		panic(err)
-   }
+	workOrder, err := openWorkOrder("digitization_work_order_report.tsv")
+	if err != nil {
+		panic(err)
+	}
 
-   for _, entry := range workOrder {
-   		fmt.Println(entry)
-   }
+	for _, entry := range workOrder {
+		fmt.Println(entry)
+	}
 }
 
 func openWorkOrder(fileLoc string) ([]WorkOrderEntry, error) {
@@ -39,10 +39,10 @@ func openWorkOrder(fileLoc string) ([]WorkOrderEntry, error) {
 	scanner := bufio.NewScanner(workOrderFile)
 	scanner.Scan() // skip the header
 
-	for scanner.Scan(){
+	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "\t")
 		workOrder = append(workOrder, WorkOrderEntry{
-			line[0],line[1],line[2],line[3], line[4], line[5], line[6], line[7],
+			line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7],
 		})
 	}
 
